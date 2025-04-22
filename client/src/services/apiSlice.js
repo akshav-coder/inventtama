952d4762cd447dc5d9d57aa9e056b3aa2f5a1792
Akshav M <akshavm@Akshavs-MacBook-Pro.local>
2025-04-22 17:18:51 +0530

    bugs

--- client/src/services/apiSlice.js
+++ client/src/services/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL || "http://localhost:5004/api",
+    baseUrl: "http://localhost:5004/api",
     prepareHeaders: (headers, { getState }) => {
       const token = getState()?.auth?.user?.token;
       if (token) {
